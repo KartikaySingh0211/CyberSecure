ce8a3fd0eb9aae6b6975e66febb728ec446f59d6
Kartikay Singh <kartikay.singh.0211@gmail.com>
2024-10-05 16:10:14 +0530

    corrected icon names in index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,11 +4,11 @@ import logo from "./logo.jpg";
 import a1 from "./a1.png";
 import a2 from "./a2.png";
 import a3 from "./a3.png";
-import iconfacebook from "./icon-facebook.svg";
-import iconinstagram from "./icon-instagram.svg";
-import iconpinterest from "./icon-pinterest.svg";
-import icontwitter from "./icon-twitter.svg";
-import iconyoutube from "./icon-youtube.svg";
+import iconfacebook from "./iconfacebook.svg";
+import iconinstagram from "./iconinstagram.svg";
+import iconpinterest from "./iconpinterest.svg";
+import icontwitter from "./icontwitter.svg";
+import iconyoutube from "./iconyoutube.svg";
 export {
 	menu,
 	close,
